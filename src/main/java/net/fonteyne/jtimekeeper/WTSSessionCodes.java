f7a1b3977288d956867952179910026a771f1c42
Eric <eric@fonteyne.net>
2025-12-12 14:32:18 +0100

    Add english comments

--- src/main/java/net/fonteyne/jtimekeeper/WTSSessionCodes.java
+++ src/main/java/net/fonteyne/jtimekeeper/WTSSessionCodes.java
@@ -1,10 +1,11 @@
 package net.fonteyne.jtimekeeper;
 
 public class WTSSessionCodes {
-    // Code du message envoyé par TS pour les changements de session
-    public static final int WM_WTSSESSION_CHANGE = 0x2B1;  // 689
 
-    // Codes WTS (wParam)
+    // This is a constant representing the session change message code in Windows Terminal Services (WTS)
+    public static final int WM_WTSSESSION_CHANGE = 0x2B1;   // 689
+
+    // These are constants representing different types of session events
     public static final int WTS_CONSOLE_CONNECT = 0x1;
     public static final int WTS_CONSOLE_DISCONNECT = 0x2;
     public static final int WTS_REMOTE_CONNECT = 0x3;
@@ -15,4 +16,3 @@ public class WTSSessionCodes {
     public static final int WTS_SESSION_UNLOCK = 0x8;
     public static final int WTS_SESSION_REMOTE_CONTROL = 0x9;
 }
-
